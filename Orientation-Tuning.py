8a30fe71859e81e25010c0da3d76a4bea43dd06c
Yann Zerlaut <yann.zerlaut@gmail.com>
2026-06-30 16:46:41 +0200

    add-fixing-protocol

--- Orientation-Tuning.py
+++ Orientation-Tuning.py
@@ -19,6 +19,15 @@ dataset = scan_folder_for_NWBfiles(\
             #for_protocols=['tuning']
             )
 
+# %%
+dFoF_parameters = dict(\
+    roi_to_neuropil_fluo_inclusion_factor=0., # no factor here
+    neuropil_correction_factor = 0.7,
+    method_for_F0 = 'sliding_percentile',
+    percentile=5., # percent
+    sliding_window = 5*60, # seconds
+)
+
 
 # %%
 dataset
