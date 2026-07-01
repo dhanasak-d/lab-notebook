849ea03b5b74032512df5c12441dd6710c2e331e
Yann Zerlaut <yann.zerlaut@gmail.com>
2026-07-01 11:52:08 +0200

    up

--- Contrast-Sensitivity.py
+++ Contrast-Sensitivity.py
@@ -12,13 +12,19 @@ from physion.utils import plot_tools as pt
 from physion.analysis.read_NWB import Data,\
     scan_folder_for_NWBfiles
 from physion.analysis.episodes.build import EpisodeData
+from physion.dataviz.episodes.trial_average import plot as plot_trial_average
 
+# TO CHANGE:
+PROTOCOL = 'tuning-high-contrast' # 'contrast-sensitivity'
 
-dataset = scan_folder_for_NWBfiles(\
-        os.path.join(os.path.expanduser('~'), 
-            'DATA', 'Taddy', 'PN_shGrid1-2026'),
-            for_protocols=['contrast-sensitivity']
-            )
+folder = os.path.join(os.path.expanduser('~'), 
+                'DATA', 'Taddy', 'PN_shGrid1-2026', 'NWBs')
+if not os.path.isdir(os.path.join(folder, 'temp')):
+    os.mkdir(os.path.join(folder, 'temp'))
+
+# %%
+dataset = scan_folder_for_NWBfiles(
+        folder, for_protocols=[PROTOCOL])
 
 # %%
 dFoF_parameters = dict(\
@@ -32,21 +38,8 @@ dFoF_parameters = dict(\
 quantity = 'dFoF'
 
 # %%
-dataset
-filename = dataset['files'][0]
-data = Data(filename)
-data.build_dFoF(**dFoF_parameters)
-for p in data.protocols[1:]:
-    print(p)
 
-    ep = EpisodeData(data, protocol_name=p)
-    print(ep.varied_parameters)
-# ep = EpisodeData(data, verbose=True, 
-#                  protocol_name=['contrast-sensitivity'])
-# %%
-
-
-def cell_sensitivity_example_fig(EPISODES,
+def cell_sensitivity_example_fig(filename,
                                  quantity=quantity,
                                 stat_test_props = dict(interval_pre=[-1,0], 
                                                        interval_post=[0,1],
@@ -64,14 +57,15 @@ def cell_sensitivity_example_fig(EPISODES,
     if quantity=='Deconvolved':
         data.build_Deconvolved()
 
-    EPISODES = EpisodeData(data,
+    ep = EpisodeData(data,
                            quantities=[quantity],
-                           protocol_name='contrast-sensitivity',
+                           protocol_name=PROTOCOL,
                            verbose=False)
 
-    fig, AX = pt.plt.subplots(Nsamples, len(EPISODES.varied_parameters['contrast']), 
+    fig, AX = pt.plt.subplots(Nsamples, 
+                              len(ep.varied_parameters['contrast']), 
                           figsize=(7,7))
-    plt.subplots_adjust(right=0.75, left=0.1, top=0.97, bottom=0.05, wspace=0.1, hspace=0.8)
+    pt.plt.subplots_adjust(right=0.75, left=0.1, top=0.97, bottom=0.05, wspace=0.1, hspace=0.8)
     
     for Ax in AX:
         for ax in Ax:
@@ -81,17 +75,17 @@ def cell_sensitivity_example_fig(EPISODES,
                                            min([Nsamples, data.nROIs]), replace=False)):
 
         # SHOW trial-average
-        plot_trial_average(EPISODES,
+        plot_trial_average(ep,
                            quantity=quantity,
                            column_key='contrast',
                            color=color,
-                           Ybar_label=1 if quantity=='dFoF' else None,
+                           Ybar=1 if quantity=='dFoF' else 0,
                            Ybar_label='1dF/F' if quantity=='dFoF' else 'a.u.',
                            Xbar=1., Xbar_label='1s',
-                           roiIndex=r,
+                           index=r,
                            with_stat_test=True,
                            stat_test_props=stat_test_props,
-                           with_screen_inset=False,
+                           with_screen_inset=True,
                            AX=[AX[i]], no_set=False)
         AX[i][0].annotate('roi #%i  ' % (r+1), (0,0), 
                           ha='right', xycoords='axes fraction')
@@ -102,13 +96,13 @@ def cell_sensitivity_example_fig(EPISODES,
         contrasts, y, sy, responsive_contrasts = [], [], [], []
         responsive = False
 
-        for c, contrast in enumerate(EPISODES.varied_parameters['contrast']):
+        for c, contrast in enumerate(ep.varied_parameters['contrast']):
 
-            stats = EPISODES.stat_test_for_evoked_responses(episode_cond=\
-                                            EPISODES.find_episode_cond(key='contrast',
+            stats = ep.stat_test_for_evoked_responses(episode_cond=\
+                                            ep.find_episode_cond(key='contrast',
                                                                        value=contrast),
                                                             response_args=dict(quantity=quantity,
-                                                                               roiIndex=r),
+                                                                               index=r),
                                                             **stat_test_props)
 
             contrasts.append(contrast)
@@ -130,16 +124,17 @@ def cell_sensitivity_example_fig(EPISODES,
         #inset.set_xticklabels(['%i'%a if (i%2==0) else '' for i, a in enumerate(contrasts)], fontsize=7)
     inset.set_xlabel('contrast', fontsize=7)
 
-        
     return fig
 
 # iSession = 0 # session index
-fig = cell_sensitivity_example_fig(ep)
-# plt.show()
-# %%
+fig = cell_sensitivity_example_fig(dataset['files'][0])
 
 
 # %%
+
+from physion.analysis.protocols.contrast_sensitivity import\
+                        compute_sensitivity_per_cells
+
 # to be a valid dataset:
 nMIN_DATAFILES = 2
 
@@ -147,9 +142,6 @@ def process_file(filename, i, c, quantity):
 
     # to be a valid datafile:
     nMIN_ROIs = 4
-
-    # CELL-dependent calcium pre-processing params 
-    dFoF_parameters = get_dFoF_params(c)
 
     # statistical test for visually-evoked-responses
     if quantity=='Deconvolved':
@@ -168,7 +160,7 @@ def process_file(filename, i, c, quantity):
 
     print('%i) ' % (i+1), 'analyzing file: %s  [...] ' % filename)
     data = Data(filename, verbose=False)
-    protocol_name=[p for p in data.protocols if '8contrast' in p][0]
+    protocol_name=[p for p in data.protocols if PROTOCOL in p][0]
     data.build_dFoF(**dFoF_parameters, verbose=False)
 
     if data.nROIs>=nMIN_ROIs:
@@ -194,7 +186,7 @@ def process_file(filename, i, c, quantity):
             Sensitivity['nROIs_final'] = data.nROIs
             Sensitivity['subject'] = data.nwbfile.subject.subject_id
 
-            np.save(os.path.join(summary_folder, 'temp', 
+            np.save(os.path.join(folder, 'temp', 
                                  'Sensitivity-%s-%s-%i.npy' % (quantity, c, i)),
                     Sensitivity)
             print('      [v] --> included, n=%i ROIs ' % data.nROIs)
@@ -210,63 +202,89 @@ def process_file(filename, i, c, quantity):
 
 # %%
 
-DATASET = scan_folder_for_NWBfiles(folder)
+groups = {
+    'shRNA':{'virus':'CamKII-Cre+shGrid1'},
+    'scramble':{'virus':'CamKII-Cre+shScramble'}
+}
 
-# FILTER
-# 1) protocol type: contrast sensitivity
-cond = np.array([np.sum(['8contrast' in p for p in protocols])\
-                for protocols in DATASET['protocols']], dtype=bool)
+parallelized = False
 
-# 2) age condition
-if datasets[c]['age_interval'] is not None:
-    cond = cond &\
-        (DATASET['ages']>=datasets[c]['age_interval'][0]) &\
-        (DATASET['ages']<=datasets[c]['age_interval'][1])
+for c in groups:
 
-if len(DATASET['files'][cond])>nMIN_DATAFILES:
+    # FILTER
+    # 1) protocol type: contrast sensitivity
+    cond = np.array([np.sum([PROTOCOL in p for p in protocols])\
+                    for protocols in dataset['protocols']], dtype=bool)
+    # 2) virus
+    cond = cond & (dataset['viruses']==groups[c]['virus'])
 
-    if parallelized:
-        ################################################
-        ###    parallelization here !   #################
-        ################################################
-        nruns = int(len(DATASET['files'][cond])/cpus)+1
+    if len(dataset['files'][cond])>nMIN_DATAFILES:
 
-        for r in range(nruns):
-            i0 = r*cpus
-            imax = np.min([i0+cpus, len(DATASET['files'][cond])]) 
-            print(' - running set of files %i:%i' % (i0, imax))
+        if parallelized:
 
-            # start the processes
-            procs = []
-            for i in range(i0,imax):
-                proc = multiprocessing.Process(\
-                                    target=process_file, 
-                                    args=(DATASET['files'][cond][i], 
-                                            i, c, quantity))
-                procs.append(proc)
-                proc.start()
+            ################################################
+            ###    parallelization here !   #################
+            ################################################
+            import multiprocessing
+            cpus = multiprocessing.cpu_count()
+            nruns = int(len(dataset['files'][cond])/cpus)+1
 
-            # complete the processes
-            for proc in procs:
-                proc.join()
-    else:
-        #####################################
-        ###### UN-PARALLELIZED VERSION ######
-        for i, f in enumerate(DATASET['files'][cond]):
-            process_file(f, i, c, quantity)
-        #####################################
+            for r in range(nruns):
+                i0 = r*cpus
+                imax = np.min([i0+cpus, len(dataset['files'][cond])]) 
+                print(' - running set of files %i:%i' % (i0, imax))
 
-    # now that we have stored all datafile outputs
-    Sensitivities = []
-    for i, f in enumerate(DATASET['files'][cond]):
+                # start the processes
+                procs = []
+                for i in range(i0,imax):
+                    proc = multiprocessing.Process(\
+                                        target=process_file, 
+                                        args=(dataset['files'][cond][i], 
+                                                i, c, quantity))
+                    procs.append(proc)
+                    proc.start()
 
-        if os.path.isfile(os.path.join(summary_folder, 'temp', 
-                                        'Sensitivity-%s-%s-%i.npy' % (quantity, c, i))):
-            Sensitivity = np.load(os.path.join(summary_folder, 'temp', 
-                                        'Sensitivity-%s-%s-%i.npy' % (quantity, c, i)),
-                                allow_pickle=True).item()
-            Sensitivities.append(Sensitivity)
+                # complete the processes
+                for proc in procs:
+                    proc.join()
+        else:
+            #####################################
+            ###### UN-PARALLELIZED VERSION ######
+            for i, f in enumerate(dataset['files'][cond]):
+                process_file(f, i, c, quantity)
+            #####################################
 
-    # # saving data
-    np.save(os.path.join(summary_folder, 'Sensitivities_%s_%s.npy' % (quantity, c)), 
-            Sensitivities)
+        # now that we have stored all datafile outputs
+        Sensitivities = []
+        for i, f in enumerate(dataset['files'][cond]):
+
+            if os.path.isfile(os.path.join(folder, 'temp', 
+                                            'Sensitivity-%s-%s-%i.npy' % (quantity, c, i))):
+                Sensitivity = np.load(os.path.join(folder, 'temp', 
+                                            'Sensitivity-%s-%s-%i.npy' % (quantity, c, i)),
+                                    allow_pickle=True).item()
+                Sensitivities.append(Sensitivity)
+
+        # # saving data
+        np.save(os.path.join(folder, 'Sensitivities_%s_%s.npy' % (quantity, c)), 
+                Sensitivities)
+
+# %%
+from physion.analysis.protocols.contrast_sensitivity\
+        import plot_contrast_sensitivity, plot_contrast_responsiveness
+fig, ax = plot_contrast_sensitivity(\
+                         ['%s_scramble' % quantity],
+                        # ['%s_scramble' % quantity, 
+                        #  '%s_shRNA' % quantity],
+                          average_by='ROIs',
+                        path=os.path.join(folder))
+# %%
+np.load(os.path.join(folder, 'Sensitivities_dFoF_scramble.npy'),
+        allow_pickle=True)[7]['Responses'].shape
+# %%
+for fn in dataset['files']:
+    data = Data(fn)
+    data.build_visual_stim()
+    print(len(data.visual_stim.experiment['time_start']))
+
+# %%
